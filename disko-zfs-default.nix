49f4fcc92152b79feee8d7a50340d17722df8d77
an3sol <willysetov@gmail.com>
2024-11-22 15:38:13 +0200

    Create disko-zfs-default.nix

---
+++ disko-zfs-default.nix
@@ -0,0 +1,66 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs-pool = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "rpool";  # Recommended to name your ZFS pool
+                # Consider adding these options
+                options = {
+                  compression = "zstd";  # Good compression with low overhead
+                  atime = "off";  # Improves performance
+                  xattr = "sa";  # Small performance improvement
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      rpool = {
+        type = "zpool";
+        # Consider these additional pool-level options
+        options = {
+          ashift = "12";  # Optimal for modern drives
+          autotrim = "on";  # Good for SSDs
+        };
+        # If you want datasets
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options = {
+              mountpoint = "legacy";  # Important for NixOS
+              canmount = "on";
+            };
+          };
+          # Optional: separate datasets for /home, /var, etc.
+          "home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options = {
+              mountpoint = "legacy";
+            };
+          };
+        };
+      };
+    };
+  };
+}
